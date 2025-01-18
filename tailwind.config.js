eea9cfc91f3ec533abb7e69cbc8341630dca9ffa
KennyKvn001 <k.mugisha@alustudent.com>
2025-01-18 03:24:03 +0200

    From Nav to Excellence section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        silver: '#333',
+        gold: '#a18a4d'
+      }
+    },
   },
   plugins: [],
 }
